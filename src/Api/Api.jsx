1f82295f1fff6d7491620c16719fe788e6b7ee67
Saidumarov <saidumarovjafarxon@gmail06.com>
2023-06-07 15:49:36 +0500

    .

---
+++ src/Api/Api.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import "./api.css"
+
+const Api = () => {
+
+
+    const [state, setState] = useState([])
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(response => response.json())
+            .then(json => {
+                setState(json)
+                console.log(state);
+
+            })
+    }, [])
+
+    function of() {
+        {
+            const divElement = document.querySelectorAll("span")
+            divElement.forEach((element) => {
+                element.classList.toggle("active")
+            });
+        }
+
+
+        {
+            const divElements = document.querySelectorAll(".of")
+            divElements.forEach((element) => {
+                element.classList.toggle("active")
+            });
+        }
+ 
+        for (let i= document.querySelector(".bb"); i = ""; i++) {
+            i.addEventListener("cilck" , () =>{
+               console.log("salom"); 
+            })
+            
+          }
+    }
+   
+
+    return (
+        <div>
+            <div className="ress">
+                {state.map(itme => (
+                    <div className="of">
+                        <div className="res">
+                            <p>{itme.body}</p> <span> ....</span>
+                            <button class="bb" onClick={of} >boss</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Api
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Api from './Api/Api'
+
+
+function App() {
+  return (
+
+    <div>
+      <Api />
+    </div>
+
+
+  )
+}
+
+export default App
